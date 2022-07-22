64a947b7d2929eaf728a3179f7577002e7303e92
VolodyaZherevchuk <109279453+VolodyaZherevchuk@users.noreply.github.com>
2022-07-22 18:26:57 +0300

    1 - 4

--- src/MarketPlace.java
+++ src/MarketPlace.java
@@ -1,11 +1,15 @@
-import java.util.Scanner;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class MarketPlace {
-
+        static  Map<String, List<String>> historyPurchaseUser = new HashMap<>();
         public static void main(String[] args) {
+
+
             Scanner scanner = new Scanner(System.in);
             Products products = new Products();
             Users users = new Users();
+
             boolean flag = true;
             while (flag){
                 System.out.println("\n\n   Menu");
@@ -26,9 +30,16 @@ public class MarketPlace {
                         products.displayListOfAllProducts();
                         break;
                     case 3:
+                        int[] array = users.buyingProduct(products);
+                        if (array[0] > -1 && array[1] > -1) {
+                            put(historyPurchaseUser, users.returnName(array[0]), products.returnNameProduct(array[1]));
+                            //historyPurchaseUser.put(array[0], array[1]);
+                            int a=1;
+
+                        }
                         break;
-                    case 4
-                        flag = false;
+                    case 4:
+                        displayListOfUserProducts(products4);
                         break;
                     case 5:
                         break;
@@ -40,30 +51,111 @@ public class MarketPlace {
 
             }
         }
+
+        public static void displayListOfUserProducts (Products products){
+
+            Scanner scanner = new Scanner(System.in);
+
+            System.out.print("Input Id user ==> ");
+            int idU = scanner.nextInt();
+            int indexIdUser = products.returnIndex(idU);
+            if (indexIdUser < 0){
+                System.out.println("User id - " + idU + " not found");
+            } else {
+                historyPurchaseUser.forEach((s, strings) -> {
+                    System.out.print(s + ": ");
+                    System.out.println(strings.stream().collect(Collectors.joining(", ")));
+                });
+            }
+
+        }
+
+    private static <KEY, VALUE> void put(Map<KEY, List<VALUE>> map, KEY key, VALUE value) {
+        map.compute(key, (s, strings) -> strings == null ? new ArrayList<>() : strings).add(value);
+    }
 }
 
 class Products {
-    int[] id = {1, 2, 3};
+    int[] idProduct = {1, 2, 3};
     String[] name = {"Acer", "ASUS", "Lenovo"};
     float[] price = {29999.99f, 22999.99f, 16999.99f};
     public void displayListOfAllProducts() {
+        System.out.println("\nlist of all products");
         for (int i = 0; i <3; i++) {
-            System.out.println(id[i] + " " + name[i] + " " + price[i]);
+            System.out.println(idProduct[i] + " " + name[i] + " " + price[i]);
         }
+    }
+    public float ReturnPrice(int id){
+        return price[id];
+    }
+
+    public  String returnNameProduct(int id){
+        return name[id];
+    }
+
+    public int returnIndex(int id){
+        return Arrays.binarySearch(idProduct, id);
     }
 }
 
 class Users {
-    int[] id = {1, 2 ,3};
+    int[] idUser = {1, 2, 3};
     String[] nameFirst = {"Ivan", "Petr", "Vasilij"};
     String[] nameLast = {"Ivanov", "Petrov", "Sidorov"};
     float[] amountOfMoney ={100000.00f, 100000.00f, 100000.00f};
 
     public void displayListOfAllUsers () {
-        System.out.println("list of all users");
+        System.out.println("\nlist of all users");
         for (int i = 0; i <3; i++) {
-            System.out.println(id[i] + " " + nameFirst[i] + " " + nameLast[i] + " " + amountOfMoney[i]);
+            System.out.println(idUser[i] + " " + nameFirst[i] + " " + nameLast[i] + " " + amountOfMoney[i]);
         }
+    }
+
+
+    public int returnIndex(int id){
+      return Arrays.binarySearch(idUser, id);
+    }
+
+    public String returnName(int id) {
+        return nameFirst[id] + " " + nameLast[id];
+    }
+
+    public int[] buyingProduct(Products products) {
+        int[] result = {0,0};
+        Scanner scanner =new Scanner(System.in);
+
+        System.out.print("Input Id user ==> ");
+        int idU = scanner.nextInt();
+        int indexIdUser = returnIndex(idU);
+        if (indexIdUser < 0){
+            System.out.println("User id - " + idU + " not found");
+            result[0] =-1;
+            return result;
+        } else {
+            result[0] = indexIdUser;
+        }
+
+        System.out.print("Input Id product ==> ");
+        int idP = scanner.nextInt();
+        int indexIdProduct = products.returnIndex(idP);
+
+        if (indexIdProduct < 0 ){
+            System.out.println("Product id - " + idP + " not found");
+            result[1] =-1;
+            return result;
+        } else {
+            result[1] = indexIdProduct;
+        }
+
+
+        float price = products.ReturnPrice(indexIdProduct);
+        if (price > amountOfMoney[indexIdUser]) {
+            System.out.println("User " + nameFirst[indexIdUser] +" does not have enough money");
+        } else {
+            System.out.println("User " + nameFirst[indexIdUser] + " to buy " + products.returnNameProduct(indexIdProduct));
+            amountOfMoney[indexIdUser] -= price;
+        }
+        return result;
     }
 
 
